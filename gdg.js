867a971f5987bebacfae1b491fe4433ec564649b
SwathiJambagi <1nt23is228.swathi@nmit.ac.in>
2025-01-03 21:59:40 +0530

    added web files.

---
+++ gdg.js
@@ -0,0 +1,9 @@
+// const navbar = document.getElementById('navbar1');
+
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 50) {
+//         navbar.classList.add('transparent'); // Make navbar transparent
+//     } else {
+//         navbar.classList.remove('transparent'); // Restore original background
+//     }
+// });
